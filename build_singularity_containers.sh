106d68af112e33fcc87a3a2103e4a58819c967db
Jérémy Rousseau <jeremy.rousseau2@outlook.fr>
2024-04-01 00:56:55 +0200

    Update build_singularity_containers.sh

--- build_singularity_containers.sh
+++ build_singularity_containers.sh
@@ -4,9 +4,9 @@
 
 start=`date`
 
+singularity build --fakeroot $PWD/containers/singularity/lagoon_mcl_1-0-0.sif $PWD/containers/singularity/lagoon_mcl_1-0-0.def
 singularity build --fakeroot $PWD/containers/singularity/seqkit_2.6.1.sif $PWD/containers/singularity/seqkit_2.6.1.def
 singularity build --fakeroot $PWD/containers/singularity/diamond_2.1.8.sif $PWD/containers/singularity/diamond_2.1.8.def
-singularity build --fakeroot $PWD/containers/singularity/lagoon_mcl_1-0-0.sif $PWD/containers/singularity/lagoon_mcl_1-0-0.def
 singularity build --fakeroot $PWD/containers/singularity/mcl_22-282.sif $PWD/containers/singularity/mcl_22-282.def
 
 end=`date`
