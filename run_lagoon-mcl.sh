804ea848367848bff046be5fb026a3904bf96f8b
Jeremy <jeremy.rousseau2@outlook.fr>
2024-05-20 20:53:47 +0200

    update run_lagoon-mcl.sh

--- run_lagoon-mcl.sh
+++ run_lagoon-mcl.sh
@@ -4,16 +4,16 @@ nextflow run main.nf -profile mamba \
     --max_cpus 15 \
     --max_memory '60.GB' \
     --max_time '336.h' \
-    --projectName LAGOON-MCL \
-    --fasta "test/full/tr_files_test/*.fasta" \
-    --annotation "test/full/an_files_test/*.tsv" \
-    --pep_colname peptides \
-    --columns_attributes database-identifiant,interproscan \
+    --projectName LAGOON-MCL_2 \
+    --fasta "test/tr/*.fasta" \
+    --annotation "test/an/*.tsv" \
+    --pep_colname protein_accession \
+    --columns_attributes analysis-signature_accession,interpro_accession \
     --concat_fasta all_sequences \
     --information true \
-    --information_files test/full/in_files_test/information_files.tsv \
-    --information_attributes Phylum_Metdb,Genus_Metdb,trophic_mode \
-    --outdir results \
+    --information_files test/dinoflagellata_v3.csv \
+    --information_attributes Phylum,Class,Order,Family,Genus,Species \
+    --outdir results_2 \
     --run_diamond true \
     --diamond_db reference \
     --alignment_file null \
@@ -25,4 +25,5 @@ nextflow run main.nf -profile mamba \
     --matrix BLOSUM62 \
     --diamond_evalue 0.001 \
     --I 1.4,2,4 \
-    --max_weight 350
+    --max_weight 350 \
+    -resume
