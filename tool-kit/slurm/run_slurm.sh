2e15e27e14bd4f0324333c9a86038efa3b6feb0f
Jérémy Rousseau <jeremy.rousseau2@outlook.fr>
2026-01-14 12:38:20 +0100

    update run_slurm.sh

--- tool-kit/slurm/run_slurm.sh
+++ tool-kit/slurm/run_slurm.sh
@@ -5,13 +5,13 @@
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=16GB
+#SBATCH --mem=24GB
 
-cd /path/to/lagoon-MCL
+cd /shared/projects/darkdino/lagoon-mcl/darkdino
 
 # This script runs LAGOON-MCL on the ABiMS compute cluster (https://abims.sb-roscoff.fr/)
 # The modules to be loaded may be different on your system
 module load nextflow slurm-drmaa graphviz
 
 # Modify the abims.config file with the file specific to your calculation cluster
-nextflow run main.nf -profile custom,singularity -c conf/abims.config -w /path/to/your/working/directory -resume
+nextflow run main.nf -profile singularity -params-file params.yaml -c conf/abims.config
